67c56123d380623adba0a60076ff65b88b544d70
Jesper Larsson <jesper.larsson@iths.se>
2025-09-20 16:51:42 +0200

    argument parsing
    Signed-off-by: Jesper Larsson <jesper.larsson@iths.se>

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -11,8 +11,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println(Pattern.matches("^--zone SE[1-4]$", "--zone SE4"));
-
         ElpriserAPI elpriserAPI = new ElpriserAPI();
         ElpriserAPI.Prisklass zon;
         //set some default values
